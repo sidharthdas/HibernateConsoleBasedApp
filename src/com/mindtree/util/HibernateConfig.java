992b4d4badaa9d647e7c6aa7898d4bd733895534
Sidharth Das <Sidharth.das2@mindtree.com>
2019-07-22 15:43:11 +0530

    Added path to download jar files

--- src/com/mindtree/util/HibernateConfig.java
+++ src/com/mindtree/util/HibernateConfig.java
@@ -1,8 +1,15 @@
 package com.mindtree.util;
 
 import org.hibernate.Session;
+
+
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+
+/*https://sourceforge.net/projects/hibernate/files/hibernate3/3.6.4.Final/
+
+	Use above url to download the hibernate jars & download mysql-connector jar*/
 
 public class HibernateConfig {
 
